2a203e43ab922e0c94b531b0c0b46116feee862a
lbpaisDev <lbpaisdev@gmail.com>
2020-03-21 21:14:26 +0000

    Some ideas

--- backtracking_lp.c
+++ backtracking_lp.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
+
+//DEBUG
 #define DEBUG 0
+
+//Run time
+clock_t start, end;
 
 //Structures
 //Listeners positions on the map
@@ -21,20 +27,35 @@ typedef struct typesAntennas{
     int r,c;
 }t;
 
+//Inicialize lists
+//Listeners
+int np;
+p *pArray;
+//Antennas
+int nn;
+n *nArray;
+//Types
+int nt;
+t *tArray;
+
 //Print Structs
+//Listeners
 void printP(p pArray){
     printf("\tListener X: %d Y: %d Covered: %d \n", pArray.x, pArray.y, pArray.covered);
 
 }
+//Antennas
 void printN(n nArray){
     printf("\tPosition X: %d Y: %d\n", nArray.x, nArray.y);
 
 }
+//Types
 void printT(t tArray){
     printf("\tAntenna R: %d C: %d\n", tArray.r, tArray.c);
 }
 
 //Print Arrays of structs
+//Listeners
 void printParr(p *pArray, int len){
     printf("Positions of the listeners:\n");
     int i;
@@ -42,6 +63,7 @@ void printParr(p *pArray, int len){
         printP(pArray[i]);
     }
 }
+//Antennas
 void printNarr(n *nArray, int len){
     printf("Positions of the antennas:\n");
     int i;
@@ -49,6 +71,7 @@ void printNarr(n *nArray, int len){
         printN(nArray[i]);
     }
 }
+//Types
 void printTarr(t *tArray, int len){
     printf("Types of antennas:\n");
     int i;
@@ -58,21 +81,39 @@ void printTarr(t *tArray, int len){
 }
 
 //Comparison function for quick sort
-int cmpfunc (const void * a, const void * b) {
+//First in order of x then y for listenners
+int cmpfuncList(const void * a, const void * b) {
+    p *tA = (p*)a;
+    p *tB = (p*)b;
+    if(((int)tA->x - (int)tB->x) == 0){
+        return (int)tA->y - (int)tB->y;
+    }
+    return ((int)tA->x - (int)tB->x);
+}
+//First in order of x then y for antennas
+int cmpfuncAnt(const void * a, const void * b) {
+    n *tA = (n*)a;
+    n *tB = (n*)b;
+    if(((int)tA->x - (int)tB->x) == 0){
+        return (int)tA->y - (int)tB->y;
+    }
+    return ((int)tA->x - (int)tB->x);
+}
+//Cost for types of antennas
+int cmpfuncTypes(const void * a, const void * b) {
     t *tA = (t*)a;
     t *tB = (t*)b;
     return ((int)tA->c - (int)tB->c);
 }
 
-//Main function
-int minimizeCost(){
+//Get problem info
+void getData(){
     int cost = 0;
     int i, j;
 
     //Get info on listeners
-    int np;
     scanf("%d", &np);
-    p *pArray = (p*)malloc(np * sizeof(p));
+    pArray = (p*)malloc(np * sizeof(p));
     for(i = 0; i < np; i++){
         scanf("%d %d", &pArray[i].x, &pArray[i].y);
         pArray[i].covered = 0;
@@ -80,22 +121,29 @@ int minimizeCost(){
 
     //Get info on antennas
     //Positions
-    int nn;
     scanf("%d", &nn);
-    n *nArray = (n*)malloc(nn * sizeof(n));
+    nArray = (n*)malloc(nn * sizeof(n));
     for(i = 0; i < nn; i++){
         scanf("%d %d", &nArray[i].x, &nArray[i].y);
     }
     //Types
-    int nt;
     scanf("%d", &nt);
-    t *tArray = (t*)malloc(nt * sizeof(t));
+    tArray = (t*)malloc(nt * sizeof(t));
     for(i = 0; i < nt; i++){
         scanf("%d %d", &tArray[i].r, &tArray[i].c);
     }
+}
 
-    //Sort types by cost
-    qsort(tArray, nt, sizeof(t), cmpfunc);
+//Cost is the var to be optimized
+int cost = 0;
+
+//Check covered listenners
+int covered = 0;
+
+//Main algorithm
+int minimizeCost(){
+    //Iterators
+    int i,j;
 
     //Check if the solution is possible
     int covered = 0;
@@ -104,7 +152,6 @@ int minimizeCost(){
             if(((pow(nArray[i].x - pArray[j].x, 2) + pow(nArray[i].y - pArray[j].y, 2)) < pow(tArray[nt-1].r, 2)) && pArray[j].covered == 0) {
                 covered++;
                 pArray[j].covered = 1;
-                cost += tArray[nt-1].c;
             }
         }
     }
@@ -117,41 +164,27 @@ int minimizeCost(){
         pArray[i].covered = 0;
     }
 
-    if(DEBUG == 1){
-        printf("\n\n ----- DEBUG ----- 	\n\n");
-        printParr(pArray, np);
-        printNarr(nArray, nn);
-        printTarr(tArray, nt);
-        printf("\n\n\n\n");
-    }
-
-    //Main algorithm
-    //Start with the cheapest antennas
+     //Start with the cheapest antennas
     int type = 0;
     covered = 0;
     while(type < nt){
-        //Pre calculate the radius of each antenna
-        double radius = pow(tArray[type].r, 2);
         //For each possible location
         for(i = 0; i < nn; i++){
             //Check how many listeners are covered by the cheapest antennas
             for(j = 0; j < np; j++){
                 //Do the math only if a listener isn't already covered
                 if(pArray[j].covered == 0){
-                    //Determine if current listener is within the radius of the antenna with a the current possible location as it center
-                    double computed = pow((nArray[i].x - pArray[j].x), 2) + pow((nArray[i].y - pArray[j].y), 2);
-
+                
                     if(DEBUG == 1){
                         printf("\n\n ----- DEBUG ----- 	\n\n");
                         printP(pArray[j]);
                         printN(nArray[i]);
                         printT(tArray[type]);
-                        printf("\tComputed: %f Radius: %f\n", computed, radius);
                         printf("\tCost: %d\n", cost);
                         printf("\n\n\n\n");
                     }
 
-                    if(computed <= radius){
+                    if(pArray[j].x <= (nArray[i].x + tArray[type].r) || pArray[j].x <= (nArray[i].x - tArray[type].r) || pArray[j].x <= (nArray[i].y + tArray[type].r) || pArray[j].x <= (nArray[i].y - tArray[type].r)){
                         covered++;
                         pArray[j].covered = 1;
                         //If it is it means we can use it and its optimal because its the cheapest
@@ -171,11 +204,35 @@ int minimizeCost(){
 }
 
 int main() {
+    //Get initial info for the problem
+    getData();
+
+    //Measure runtime
+    start = clock();
+
+    //Sort lists
+    qsort(pArray, np, sizeof(p), cmpfuncList);
+    qsort(nArray, nn, sizeof(n), cmpfuncAnt);
+    qsort(tArray, nt, sizeof(t), cmpfuncTypes);
+
+    if(DEBUG == 1){
+        printParr(pArray, np);
+        printNarr(nArray, nn);
+        printTarr(tArray, nt);
+    }
+
+    //Final cost
     int finalCost = minimizeCost();
     if(finalCost < 0){
         printf("no solution\n");
     }else{
         printf("%d\n", finalCost);
+    }
+    
+    //Measure runtime
+    end = clock();
+    if(DEBUG == 1){
+        printf("\n---Runtime: %f\n", ((double)(end - start)/CLOCKS_PER_SEC));
     }
     return 0;
 }
